f8b2aa7de65b0d1acdcc85b2fe8c3e0fe883b0c2
jbarinas-01 <jbarinas@jonahgroup.com>
2019-11-09 10:28:25 -0500

    feature/EW-003: refactoring WalletConnect component

--- frontend/src/views/walletconnect/WalletConnect.jsx
+++ frontend/src/views/walletconnect/WalletConnect.jsx
@@ -3,14 +3,16 @@ import {connect} from "react-redux";
 
 import * as actions from "../../store/walletconnect/walletConnectActions";
 
-let WalletConnect = props => {
-  return (
-    <section>
-      <button onClick={props.connect}>connect wallet</button>
-      <div>{JSON.stringify(props.walletConnect)}</div>
-    </section>
-  );
-};
+class WalletConnect extends React.Component {
+  render() {
+    return (
+      <section>
+        <button onClick={this.props.connect}>connect wallet</button>
+        <div>{JSON.stringify(this.props.walletConnect)}</div>
+      </section>
+    );
+  }
+}
 
 const mapStateToProps = state => ({walletConnect: state.walletConnect});
 
