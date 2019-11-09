96f8e1d694a2cc444e4683c1cb083f2fdaed6d7a
ChrisGivelas <chris_g_50@hotmail.com>
2019-11-09 13:07:48 -0500

    Add dFuse API + method for querying for a key transfer

---
+++ frontend/src/api/dFuse.js
@@ -0,0 +1,37 @@
+import { createDfuseClient } from "@dfuse/client";
+
+const dFuseClient = createDfuseClient({
+  apiKey: "web_47d20cf6a437bc135a12b9b613db5bb4",
+  network: "mainnet.eth.dfuse.io",
+});
+
+export const query = async (operation) => {
+    let message = await dFuseClient.graphql(operation);
+    console.log(message);
+    return message
+};
+
+const checkWalletForKeyQuery = (userAddr, contractAddr) => `query{
+    searchTransactions(indexName: LOGS, query: "topic.2:${userAddr} topic.0:0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef address:${contractAddr}",lowBlockNum: 8647393, highBlockNum: -1, limit:1){
+      edges {
+        undo
+        node {
+          from
+          to
+          allLogs {
+            topics
+            address
+            data
+          }
+        }
+      }
+    }
+  }`
+
+export const checkWalletForKey = async (userAddr, contractAddr) => {
+    let operation = checkWalletForKeyQuery(userAddr, contractAddr);
+
+    return await query(operation);
+}
+
+export default dFuseClient;
