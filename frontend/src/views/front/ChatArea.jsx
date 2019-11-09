b85fd3b537d4574cc534092b7ddba13a4fda2b59
Charlie Morris <chtmorris@icloud.com>
2019-11-09 16:36:02 -0500

    Create overlay component

---
+++ frontend/src/views/front/ChatArea.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ChatArea = props => {
+  return (
+    <div id="content">
+      <p>yo</p>
+    </div>
+  )
+}
+
+export default ChatArea;
+
+
+
---
+++ frontend/src/views/front/LoginOverlay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginOverlay = props => {
+  return (
+    <div id="LoginOverlay">
+
+    </div>
+  )
+}
+
+export default LoginOverlay;
