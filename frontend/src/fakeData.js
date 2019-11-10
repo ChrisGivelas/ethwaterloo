f9cc07c25c644a738bd09d3c5dfcfc4a3fd50e28
ChrisGivelas <chris_g_50@hotmail.com>
2019-11-10 04:16:35 -0500

    add fake data

--- frontend/src/fakeData.js
+++ frontend/src/fakeData.js
@@ -1,20 +1,41 @@
-const faker = require('faker');
+import faker from 'faker';
+
+import user1Img from './views/front/img/user1.png';
+import user2Img from './views/front/img/user2.png';
 
 const ETHBoston = "ETHBoston";
 const ETHDenver = "ETHDenver";
 const ETHWaterloo = "ETHWaterloo";
 
+const user1 = {
+    src: user1Img,
+    className: "user1",
+    addr: "0x005b661B34Cb13E4023A26048eB6440a9e1d611B"
+};
+
+const user2 = {
+    src: user2Img,
+    className: "user2",
+    addr: "0xB0ad425cA5792DD4C4Af9177c636e5b0e6c31999"
+};
 
 export const generateFakeRoom = (roomName, size) => {
     let room = {roomName, history: []};
 
-    for(var i = 0; i < size; i++){
-        room.history.push({
-            src: null,
-            addr: null,
-            timestamp: null,
-            text: null
-        });
+    for(var i = 0; i < size; i++) {
+        if(i % 2 === 0){
+            room.history.push({
+                ...user1,
+                timestamp: faker.date.past(),
+                text: faker.lorem.sentence()
+            });
+        } else {
+            room.history.push({
+                ...user2,
+                timestamp: faker.date.past(),
+                text: faker.lorem.sentence()
+            });
+        }
     }
 
     return room;
--- frontend/src/views/main/Main.jsx
+++ frontend/src/views/main/Main.jsx
@@ -7,20 +7,16 @@ import Torus from "../torus/Torus";
 import WalletConnect from "../walletconnect/WalletConnect";
 import ChatArea from "../front/ChatArea";
 
-import fakeData from '../../fakeData';
-
-console.log(fakeData);
-
 class Main extends React.Component {
   render() {
     return (
       <main>
         <Switch>
-          <Route path="/" component={Front} />
-          <Route path="/torus" component={Torus} />
-          <Route path="/walletconnect" component={WalletConnect} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/room/:roomName" component={ChatArea}/>
+          <Route exact path="/" component={Front} />
+          <Route exact path="/torus" component={Torus} />
+          <Route exact path="/walletconnect" component={WalletConnect} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/room/:roomName" component={ChatArea}/>
 
           <Redirect to="/" />
         </Switch>
