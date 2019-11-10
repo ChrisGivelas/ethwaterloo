3a26a02cc1aad1ee07ae70b223bbbc0ff02e593f
Charlie Morris <chtmorris@icloud.com>
2019-11-09 21:29:50 -0500

    Added button

--- frontend/src/views/dashboard/Dashboard.jsx
+++ frontend/src/views/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from 'rimble-ui';
+import { Card, Button } from 'rimble-ui';
 
 class Dashboard extends React.Component {
   render() {
@@ -23,16 +23,9 @@ class Dashboard extends React.Component {
           </svg>
         </p>
           <Card width={"420px"} mx={"auto"} px={4}>
-            ETHWaterloo
+            <p>ETHWaterloo</p>
+            <Button>Enter room</Button>
           </Card>
-          {/* <div className="card" style="width: 18rem;">
-            <img src="..." className="card-img-top" alt="..."></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div> */}
       </div>
     )
   }
