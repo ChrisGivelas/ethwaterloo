b70fecfc33ca0d7efb0f8cdbefb791969e43e4a4
jbarinas-01 <jbarinas@jonahgroup.com>
2019-11-09 21:10:36 -0500

    feature/login/001: connect login with torus

--- frontend/src/views/front/Front.jsx
+++ frontend/src/views/front/Front.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import {connect} from "react-redux";
+
 import Sidebar from "./Sidebar";
 import TopNav from "./TopNav";
 import ChatArea from "./ChatArea";
 import LoginOverlay from "./LoginOverlay";
 
-const Front = props => {
-  return (
-    <section>
-      <div className="wrapper">
-        <LoginOverlay></LoginOverlay>
-        <Sidebar></Sidebar>
-        <TopNav></TopNav>
-        <ChatArea></ChatArea>
-      </div>
-    </section>
-  );
-};
+class Front extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="wrapper">
+          {this.props.torus.isConnected && <LoginOverlay />}
+          <Sidebar></Sidebar>
+          <TopNav></TopNav>
+          <ChatArea></ChatArea>
+        </div>
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = state => ({torus: state.torus});
+
+const mapDispatchToProps = dispatch => ({});
+
+Front = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Front);
 
 export default Front;
